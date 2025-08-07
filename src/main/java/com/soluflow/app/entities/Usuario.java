0fcf42c5970a5aea64a2a7857541965d257ba0eb
Mauricio Santiago <mauriciosantiago95@hotmail.com>
2025-08-07 19:12:44 -0300

    adicionado h2 database, perfil test e JPA

--- src/main/java/com/soluflow/app/entities/Usuario.java
+++ src/main/java/com/soluflow/app/entities/Usuario.java
@@ -1,14 +1,19 @@
 package com.soluflow.app.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_usuario")
 public class Usuario implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
